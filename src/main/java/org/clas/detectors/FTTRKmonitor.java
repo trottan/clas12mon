a8f99b98c05b09d1d2aaebd3a136f65706b40e1c
Guillaum-C <guillaum@jlab.org>
2018-10-19 18:38:17 +0200

    Update FTT
    Update time of max plots and load daq constants from CCDB

--- src/main/java/org/clas/detectors/FTTRKmonitor.java
+++ src/main/java/org/clas/detectors/FTTRKmonitor.java
@@ -1,25 +1,57 @@
 package org.clas.detectors;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.clas.viewer.DetectorMonitor;
+import org.jlab.detector.calib.utils.ConstantsManager;
 import org.jlab.groot.data.H1F;
 import org.jlab.groot.data.H2F;
 import org.jlab.groot.group.DataGroup;
 import org.jlab.io.base.DataBank;
 import org.jlab.io.base.DataEvent;
+import org.jlab.utils.groups.IndexedTable;
 
 
 public class FTTRKmonitor  extends DetectorMonitor {  
-    
+	int sparseSample;
+	int numberOfSamples;
+	int samplingTime;
+
     private int nstrip = 768;
     private int nlayer = 4;
     private int numberOfStripsPerChip = 64 ;
     private int numberOfChips = 12;
     
+    int runNumber;
+    int defaultRunNumber=2284;
+    
     public FTTRKmonitor(String name) {
         super(name);
         
-        this.setDetectorTabNames("Occupancies_2D", "Occupancies_1D", "Average Time Maximum", "ADC and time spectra");
+        this.loadConstantsFromCCDB(defaultRunNumber);
+		
+        
+        this.setDetectorTabNames("Occupancies_2D", "Occupancies_1D", "TimeMax", "ADC and time spectra");
         this.init(false);
+    }
+    
+    public void loadConstantsFromCCDB(int runNumber){
+    	
+    	ConstantsManager  fitterManager      = new ConstantsManager();
+		List<String>  tablesFitter            = null;
+	    List<String>  keysFitter              = null;
+			        
+	    keysFitter   = Arrays.asList(new String[]{"FTTconfig"});
+		tablesFitter = Arrays.asList(new String[]{"/daq/config/fttrk"});
+		fitterManager.init(keysFitter, tablesFitter);
+
+		IndexedTable bmtConfig = fitterManager.getConstants(runNumber, "FTTconfig");
+        
+		this.sparseSample = bmtConfig.getIntValue("sparse", 0, 0 ,0);
+		this.numberOfSamples = (bmtConfig.getIntValue("number_sample", 0, 0 ,0) -1) * (this.sparseSample+1)+1;
+		this.samplingTime = (byte) bmtConfig.getDoubleValue("sampling_time", 0, 0, 0);
+		
     }
 
     @Override
@@ -46,26 +78,17 @@ public class FTTRKmonitor  extends DetectorMonitor {
             occADCl.setTitleY("Counts");
             occADCl.setFillColor(38);
             occADCl.setTitle("layer " + ilayer);
-            H1F timeMax = new H1F("timeMax_layer" + ilayer, "timeMax_layer"  + ilayer, numberOfChips, 0.5, numberOfChips+0.5);
-            timeMax.setTitleX("chip");
-            timeMax.setTitleY("Counts");
-            timeMax.setFillColor(4);
-            timeMax.setTitle("layer " + ilayer);            
-            H1F timeMaxTmp1 = new H1F("timeMaxTmp1_layer" + ilayer, "timeMaxTmp1_layer"  + ilayer, numberOfChips, 0.5, numberOfChips+0.5);
-            timeMaxTmp1.setTitleX("chip");
-            timeMaxTmp1.setTitleY("Counts");
-            timeMaxTmp1.setFillColor(4);
-            timeMaxTmp1.setTitle("layer " + ilayer); 
-            H1F timeMaxTmp2 = new H1F("timeMaxTmp2_layer" + ilayer, "timeMaxTmp2_layer"  + ilayer, numberOfChips, 0.5, numberOfChips+0.5);
-            timeMaxTmp2.setTitleX("chip");
-            timeMaxTmp2.setTitleY("Counts");
-            timeMaxTmp2.setFillColor(4);
-            timeMaxTmp2.setTitle("layer " + ilayer);             
-            DataGroup dg = new DataGroup(1,2);
+            
+            int sector=1;
+            H1F timeMaxHisto = new H1F("TimeOfMax : Layer " + ilayer + " Sector " + sector, "TimeOfMax : Layer " + ilayer + " Sector " + sector,
+					samplingTime*numberOfSamples, 1.,samplingTime*numberOfSamples );
+			timeMaxHisto.setTitleX("Time of max (Layer " + ilayer + " Sector " + sector+")");
+			timeMaxHisto.setTitleY("Nb hits");
+			timeMaxHisto.setFillColor(4);
+			
+			DataGroup dg = new DataGroup(1,2);
             dg.addDataSet(occADCl, 0);
-            dg.addDataSet(timeMax, 1);
-            dg.addDataSet(timeMaxTmp1, 1);
-            dg.addDataSet(timeMaxTmp2, 1);
+            dg.addDataSet(timeMaxHisto, 1);
             this.getDataGroup().add(dg,0,ilayer,0);
         }
        
@@ -99,13 +122,15 @@ public class FTTRKmonitor  extends DetectorMonitor {
             this.getDetectorCanvas().getCanvas("Occupancies_1D").cd(0 + ilayer -1);
             this.getDetectorCanvas().getCanvas("Occupancies_1D").draw(this.getDataGroup().getItem(0,ilayer,0).getH1F("occADC_layer" + ilayer));
         }
-        this.getDetectorCanvas().getCanvas("Average Time Maximum").divide(2, 2);
-        this.getDetectorCanvas().getCanvas("Average Time Maximum").setGridX(false);
-        this.getDetectorCanvas().getCanvas("Average Time Maximum").setGridY(false);
-        for(int ilayer=1; ilayer<=nlayer; ilayer++) {        
-            this.getDetectorCanvas().getCanvas("Average Time Maximum").cd(0 + ilayer -1);
-            this.getDetectorCanvas().getCanvas("Average Time Maximum").draw(this.getDataGroup().getItem(0,ilayer,0).getH1F("timeMax_layer" + ilayer));
-        }
+        this.getDetectorCanvas().getCanvas("TimeMax").divide(nlayer/2, nlayer/2);
+		this.getDetectorCanvas().getCanvas("TimeMax").setGridX(false);
+		this.getDetectorCanvas().getCanvas("TimeMax").setGridY(false);
+		for(int ilayer=1; ilayer<=nlayer; ilayer++) {        
+            this.getDetectorCanvas().getCanvas("TimeMax").cd(0 + ilayer -1);
+            int sector=1;
+            this.getDetectorCanvas().getCanvas("TimeMax").draw(this.getDataGroup().getItem(0,ilayer,0).getH1F("TimeOfMax : Layer " + ilayer + " Sector " + sector));
+		}
+		this.getDetectorCanvas().getCanvas("TimeMax").update();
         this.getDetectorCanvas().getCanvas("ADC and time spectra").divide(1, 2);
         this.getDetectorCanvas().getCanvas("ADC and time spectra").setGridX(false);
         this.getDetectorCanvas().getCanvas("ADC and time spectra").setGridY(false);
@@ -120,56 +145,69 @@ public class FTTRKmonitor  extends DetectorMonitor {
 
     @Override
     public void processEvent(DataEvent event) {
-
+	
         if (this.getNumberOfEvents() >= super.eventResetTime_current[10] && super.eventResetTime_current[10] > 0){
             resetEventListener();
         }
         
+        if(this.runNumber==0){
+			int numberOfEvents=this.getNumberOfEvents();
+			if(event.hasBank("RUN::config")){
+				DataBank head = event.getBank("RUN::config");
+				runNumber = head.getInt("run", 0);
+			} else {
+				runNumber = 2284;
+			}
+			this.loadConstantsFromCCDB(runNumber);
+			this.createHistos();
+			this.plotHistos();
+			this.setNumberOfEvents(numberOfEvents); //Cause number of events got reset
+		}
+        
         // process event info and save into data group
         
         if(event.hasBank("FTTRK::adc")==true){
-	    DataBank bank = event.getBank("FTTRK::adc");
-	    int rows = bank.rows();
-	    for(int loop = 0; loop < rows; loop++){
-                int sector  = bank.getByte("sector", loop);
-                int layer   = bank.getByte("layer", loop);
-                int comp    = bank.getInt("component", loop);
-                int order   = bank.getByte("order", loop);
-                int adc     = bank.getInt("ADC", loop);
-                float time  = bank.getFloat("time", loop);
-                int channel = comp + (layer-1)*nstrip;
-                int dream   = ((int) comp/this.numberOfStripsPerChip) + 1;
-                        
+        	DataBank bank = event.getBank("FTTRK::adc");
+        	int rows = bank.rows();
+        	for(int loop = 0; loop < rows; loop++){
+        		int sector  = bank.getByte("sector", loop);
+        		int layer   = bank.getByte("layer", loop);
+        		int comp    = bank.getInt("component", loop);
+        		int order   = bank.getByte("order", loop);
+        		int adc     = bank.getInt("ADC", loop);
+        		float timeOfMax  = bank.getFloat("time", loop);
+        		int channel = comp + (layer-1)*nstrip;
+                   
+        		if ((samplingTime<timeOfMax)&&(timeOfMax<samplingTime*(numberOfSamples-1))){
+        			this.getDataGroup().getItem(0, layer, 0).getH1F("TimeOfMax : Layer " + layer + " Sector " + sector).fill(timeOfMax);
+        		}
+                
 //                System.out.println("ROW " + loop + " SECTOR = " + sector + " LAYER = " + layer + " COMPONENT = " + comp +
 //                      " ADC = " + adc); 
-                if(adc>0) {
+        		if(adc>0) {
                     
-                        this.getDataGroup().getItem(0,0,0).getH2F("occADC_2D").fill(comp*1.0, layer*1.0);
+        			this.getDataGroup().getItem(0,0,0).getH2F("occADC_2D").fill(comp*1.0, layer*1.0);
                         
-                        this.getDataGroup().getItem(0,layer,0).getH1F("occADC_layer" + layer).fill(comp*1.0);
-                        this.getDataGroup().getItem(0,layer,0).getH1F("timeMaxTmp1_layer" + layer).fill(dream*1.0,1.0);
-                        this.getDataGroup().getItem(0,layer,0).getH1F("timeMaxTmp2_layer" + layer).fill(dream*1.0,time);
-                        
-                        this.getDataGroup().getItem(0,0,0).getH2F("adc").fill(adc*1.0,channel*1.0);
-                        this.getDataGroup().getItem(0,0,0).getH2F("tdc").fill(time*1.0,channel*1.0);
-                }
-                this.getDetectorSummary().getH1F("summary").fill(channel*1.0);
-	    }
-    	}
-                
-                
+        			this.getDataGroup().getItem(0,layer,0).getH1F("occADC_layer" + layer).fill(comp*1.0);
+                         
+        			this.getDataGroup().getItem(0,0,0).getH2F("adc").fill(adc*1.0,channel*1.0);
+        			this.getDataGroup().getItem(0,0,0).getH2F("tdc").fill(timeOfMax*1.0,channel*1.0);
+        		}
+        		this.getDetectorSummary().getH1F("summary").fill(channel*1.0);
+        	}
+        }
     }
 
     @Override
     public void timerUpdate() {
         if(this.getNumberOfEvents()>0) {
             for(int layer=1; layer <=nlayer; layer++) {
-                H1F raw1 = this.getDataGroup().getItem(0,layer,0).getH1F("timeMaxTmp1_layer"+layer);
-                H1F raw2 = this.getDataGroup().getItem(0,layer,0).getH1F("timeMaxTmp2_layer"+layer);
-                H1F ave = this.getDataGroup().getItem(0,layer,0).getH1F("timeMax_layer"+layer);
-                for(int loop = 0; loop < raw1.getDataSize(0); loop++){
-                    ave.setBinContent(loop, raw2.getBinContent(loop)/raw1.getBinContent(loop));
-                }
+//                H1F raw1 = this.getDataGroup().getItem(0,layer,0).getH1F("timeMaxTmp1_layer"+layer);
+//                H1F raw2 = this.getDataGroup().getItem(0,layer,0).getH1F("timeMaxTmp2_layer"+layer);
+//                H1F ave = this.getDataGroup().getItem(0,layer,0).getH1F("timeMax_layer"+layer);
+//                for(int loop = 0; loop < raw1.getDataSize(0); loop++){
+//                    ave.setBinContent(loop, raw2.getBinContent(loop)/raw1.getBinContent(loop));
+//                }
             }
         }
 
